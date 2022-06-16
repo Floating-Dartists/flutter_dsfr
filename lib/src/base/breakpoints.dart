90a5ae35baa09b211e4701022313bd7841130f77
Guillaume Roux <rouxguillaume8@gmail.com>
2022-06-16 20:32:28 +0200

    added breakpoints

---
+++ lib/src/base/breakpoints.dart
@@ -0,0 +1,10 @@
+/// Breakpoints value for different screen sizes.
+///
+/// Anything above [Breakpoints.lg] (>= 1248.0) will be considered as being
+/// "xl".
+abstract class Breakpoints {
+  static const xs = 575.0;
+  static const sm = 767.0;
+  static const md = 991.0;
+  static const lg = 1247.0;
+}
