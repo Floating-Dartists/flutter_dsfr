35edb9509732a339682e0045b3488a55c38c5eb0
Guillaume Roux <rouxguillaume8@gmail.com>
2022-07-05 15:39:38 +0200

    added golden tests for typography

--- test/theme/typography_test.dart
+++ test/theme/typography_test.dart
@@ -1,1 +1,21 @@
-void main() {}
+import 'package:alchemist/alchemist.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dsfr/flutter_dsfr.dart';
+
+void main() {
+  goldenTest(
+    'DSFRTypography.regular',
+    fileName: 'typography',
+    builder: () => GoldenTestGroup(
+      children: DSFRTypography.regular()
+          .props
+          .map<GoldenTestScenario>(
+            (e) => GoldenTestScenario(
+              name: e.name,
+              child: Text('Hello world!', style: e.property),
+            ),
+          )
+          .toList(),
+    ),
+  );
+}
